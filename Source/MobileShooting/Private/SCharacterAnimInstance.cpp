e2bdf1054b85818d332558ad11851467d10a5047
chocobe <mjh2903@naver.com>
2021-09-09 23:39:12 +0900

    AnimInstance 추가

---
+++ Source/MobileShooting/Private/SCharacterAnimInstance.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCharacterAnimInstance.h"
+
+USCharacterAnimInstance::USCharacterAnimInstance()
+{
+
+}
+
+void USCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+}
+
+void USCharacterAnimInstance::PlayNormalAttack()
+{
+	// TODO
+}
---
+++ Source/MobileShooting/Public/SCharacterAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBILESHOOTING_API USCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	USCharacterAnimInstance();
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	virtual void PlayNormalAttack();
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Attack", Meta = (AllowPrivateAccess = true))
+	UAnimMontage* NormalAttackMontage;
+};
