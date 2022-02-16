968471629ea3e322fc083c7fd16206803e220ac4
chocoberu <43817454+chocoberu@users.noreply.github.com>
2022-02-16 23:56:36 +0900

    TestSaveGame 클래스 생성

---
+++ Source/MobileShooting/Public/TestSaveGame.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TestSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBILESHOOTING_API UTestSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+public:
+
+	UTestSaveGame();
+
+};
